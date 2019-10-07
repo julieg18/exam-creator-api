df9b5cdd883e2d170f52b5fd6d38a488c9cbc060
julieg18 <julieheartcrochet@gmail.com>
2019-10-07 15:33:45 -0500

    set up env variables connect to database

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 const examRoutes = require('./routes/exams/routes');
 const userRoutes = require('./routes/users/routes');
 
 const app = express();
+
+dotenv.config();
+
+const uri = process.env.DB;
+mongoose
+  .connect(uri, {
+    dbName: 'exam-creator',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected!'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(bodyParser.json());
 
 app.use('/api/v1/exams', examRoutes);
 app.use('/api/v1/users', userRoutes);
